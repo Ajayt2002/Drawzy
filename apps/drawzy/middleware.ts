2e06ccccd025b533cb3670ca6e72641d6fe124fd
Ajay T <ajayt2002@gmail.com>
2025-12-25 10:37:17 +0000

    Initial commit

---
+++ apps/drawzy/middleware.ts
@@ -0,0 +1,15 @@
+import { withAuth } from "next-auth/middleware";
+
+
+export default withAuth({
+  pages: {
+    signIn: "/auth/signin",
+  },
+});
+
+export const config = {
+  matcher: [
+    "/chat-room/:path*",
+    "/settings/:path*"
+  ]
+}
